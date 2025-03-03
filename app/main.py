8558713bac17ccc30fc8479c39902ab1a7aad594
Pablo Pedreros <pablo.pedreros@caseware.com>
2025-03-02 21:56:01 -0500

    Removed .idea/workspace.xml from tracking

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
-from app.routes import users, schedules, meetings, reminders, analytics
-from app.database import Base, engine
-
-# Crear las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
+from app.routes import schedules, meetings, reminders, analytics, document_routes, user_routes, task_routes
 
 # Inicializar la aplicación FastAPI
-app = FastAPI(title="Group33 Backend", version="1.0")
+app = FastAPI(title="Group33 Backend")
 
 # Incluir las rutas
-app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(schedules.router, prefix="/schedules", tags=["Schedules"])
 app.include_router(meetings.router, prefix="/meetings", tags=["Meetings"])
 app.include_router(reminders.router, prefix="/reminders", tags=["Reminders"])
 app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
+app.include_router(user_routes.router)
+app.include_router(task_routes.router)
+app.include_router(document_routes.router)
 
 @app.get("/")
 def root():
