7321e803c4d0081821a5bfc52ec2fd90aac97e19
nruizp2 <n.ruizp2@uniandes.edu.co>
2025-04-18 12:52:34 -0500

    database change

--- app/database.py
+++ app/database.py
@@ -1,12 +1,16 @@
-#File for connecting to the database
-from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+# Carga las variables de entorno desde el archivo .env
+load_dotenv()
 
 # MongoDB connection URI
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 
 # Database name
 DB_NAME = "universe_app"
 
-# Create a new MongoDB client
+# Crear cliente MongoDB
+from motor.motor_asyncio import AsyncIOMotorClient
 client = AsyncIOMotorClient(MONGO_URI)
 database = client[DB_NAME]
