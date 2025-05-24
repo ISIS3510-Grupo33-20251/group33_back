1082bfbec9c10f8028643831c9f10de18ab2f9e5
juanamb <j.mejia17@uniandes.edu.co>
2025-05-23 23:15:53 -0500

    cambio entity

--- app/models/reminder.py
+++ app/models/reminder.py
@@ -17,10 +17,13 @@ class Reminder(BaseModel):
     reminder_id: str = Field(default=None, alias="_id")
     user_id: str  # The user who will receive the reminder
     entity_type: Literal["task", "meeting", "custom"]  # The type of entity the reminder is linked to
-    entity_id: Optional[str] = None
+    entity_id: Optional[str] = Field(default=None)  
     remind_at: datetime  # When the reminder should trigger
     status: ReminderStatus  # Status of the reminder
 
     class Config:
         from_attributes = True
         populate_by_name = True
+        fields = {
+            'entity_id': {'exclude': False} 
+        }
